9de33d5c0de3e8f2c5ec3b76361c7c85de3bb066
Michael Baudino <michael.baudino@alpine-lab.com>
2013-04-10 11:15:24 +0200

    Added sitemap generator

--- config.rb
+++ config.rb
@@ -28,6 +28,8 @@
 #   page "/admin/*"
 # end
 
+page '/sitemap.xml', :layout => false
+
 # Proxy (fake) files
 # page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
 #   @which_fake_page = "Rendering a fake page with a variable"
