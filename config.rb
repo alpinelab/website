e76feb448a5f10c2094e07a685a4911eb3a8ab9c
Spharian <s.fertons@gmail.com>
2015-04-15 17:39:56 +0200

    Fix assets config

--- config.rb
+++ config.rb
@@ -65,11 +65,9 @@ helpers do
   end
 end
 
-set :css_dir, 'css'
-
-set :js_dir, 'js'
-
-set :images_dir, 'img'
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
 
 set :markdown_engine, :redcarpet
 set :markdown,
